65f860e41d7f6d7ae2056efc5e02bb30cc1a7ea1
shivshank <shivshank@users.noreply.github.com>
2015-01-23 23:44:09 -0500

    Add methods for creating and linking OpenGL Programs in Pipeline

--- src/shivshank/pipeline/Pipeline.java
+++ src/shivshank/pipeline/Pipeline.java
@@ -1,5 +1,8 @@
 package shivshank.pipeline;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
@@ -59,88 +62,10 @@ public class Pipeline {
     }
     
         /**
-         * Used to reference the layout and storage of vertex shader inputs.
-         * <p/>
-         * Shader inputs are also known as vertex attributes. A ShaderInput 
-         * is independent of its source. Buffer bindings are created at capture
-         * time.
-         * <p/>
-         * The state of a <code>ShaderInput</code> is typically used for calls to
-         * glVertexAttribPointer.
-         */
-	public static class ShaderInput {
-		
-        private final int comps;
-        private final int type;
-        private final int offset;
-        private final int stride;
-        private final boolean normalized;
-        private int index;
-        
-            /**
-             * Densely packed ShaderInput constructor.
-             */
-        public ShaderInput(int numComponents, int compType) {
-            this(numComponents, compType, 0, 0, false);
-        }
-        
-            /**
-             * Custom Shader Input source layout
-             */
-        public ShaderInput(int numComponents, int compType, int offset,
-                           int stride, boolean normalized) {
-            comps = numComponents;
-            type = compType;
-            this.offset = offset;
-            this.stride = stride;
-            this.normalized = normalized;
-        }
-        
-            /**
-             * Assign an index; prepare this ShaderInput for capture.
-             * <p/>
-             * This must be called before capture.
-             *
-             * @return this for assignment convenience
-             */
-        public ShaderInput create(int index) {
-            this.index = index;
-            return this;
-        }
-        
-            /**
-             * Invalidate this object for capture; set index to 0.
-             */
-        public void destroy() {
-            index = 0;
-        }
-        
-            /**
-             * Enable relevant shader input in OpenGL.
-             */
-        public void enable() {
-            glEnableVertexAttribArray(index);
-            glVertexAttribPointer(index, comps, type,
-                                       normalized, offset, stride);
-        }
-        
-            /**
-             * Disable relevant shader input in OpenGL.
-             */
-        public void disable() {
-            glDisableVertexAttribArray(index);
-        }
-        
-        public boolean isCreated() {
-            return index != 0;
-        }
-	}
-    
-        /**
          * @throws PipelineException if shader allocation or compilation fails.
          */
 	public static class Shader {
-		private int glName;
+		protected int glName;
         private int glShaderType;
         private boolean autoDelete;
         
@@ -180,9 +105,57 @@ public class Pipeline {
             glDeleteShader(glName);
         }
         
-        protected boolean autoDestroy() {
+        boolean autoDestroy() {
             return autoDelete;
         }
 	}
 	
+    private int glName;
+    private Shader[] shaders;
+    
+    public Pipeline(Shader ... shaders) {
+        this.shaders = shaders;
+    }
+    
+    public void create() {
+        glName = glCreateProgram();
+        
+        if (glName == 0) {
+            throw new PipelineException("ERROR: OpenGL failed to create "
+                                      + "Program.");
+        }
+        
+        for (Shader c : shaders) {
+            if (!c.isCreated()) {
+                throw new PipelineException("ERROR: Shader must be created "
+                                          + "before program creation!");
+            }
+            glAttachShader(glName, c.glName);
+            if (c.autoDestroy())
+                c.destroy();
+        }
+        
+        glLinkProgram(glName);
+        int status = glGetProgrami(glName, GL_LINK_STATUS);
+        if (status == GL_FALSE) {
+            String log = glGetProgramInfoLog(glName);
+            destroy();
+            throw new PipelineException("ERROR: Program linking failed:\n"
+                         + log);
+        }
+        
+        for (Shader c : shaders) {
+            glDetachShader(glName, c.glName);
+        }
+    }
+    
+    public void destroy() {
+        glDeleteProgram(glName);
+        glName = 0;
+        shaders = null;
+    }
+    
+    public void draw(Model m) {
+        // TODO
+    }
 }
